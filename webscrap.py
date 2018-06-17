bb47fad8a0ea7cafc3f39804035e1708c5c00764
sampathkumarv1201 <sampathvaduga@gmail.com>
2018-06-17 18:39:54 +0530

    first file

---
+++ webscrap.py
@@ -0,0 +1,14 @@
+import urllib2 
+from bs4 import BeautifulSoup
+page_link='http://results.kongu.edu/allresout.php?regno=15CSR178-ODD_2017-NOVEMBER_2017'
+page=urllib2.urlopen(page_link)
+soup=BeautifulSoup(page,'html.parser')
+#print(soup)
+name=soup.find_all("th")
+'''for n in name:
+	print(n)'''
+print(name[4].text)
+print(name[5].text)
+print(name[6].text)
+print(name[28].text)
+print(name[30].text)
